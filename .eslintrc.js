21710c2ee6f6c93a70544c4b35c023b20162aedc
Vitalex951 <vitalex951@gmail.com>
2023-02-27 11:29:15 +0300

    add fix CRLF and CR files

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'arrow-body-style': 'off',
+        'linebreak-style': ['error', 'windows'],
         'i18next/no-literal-string': ['error', {
             markupOnly: true,
             ignoreAttribute: ['data-testid', 'to'],
