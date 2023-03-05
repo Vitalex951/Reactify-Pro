6b534785e79cd01f1758d67aeda5e663da63963a
Vitalex951 <vitalex951@gmail.com>
2023-03-05 14:30:23 +0300

    delete LoginModal.scss

--- src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from 'features/AuthByUserName/ui/LoginForm/LoginForm';
-import s from './LoginModal.module.scss';
 
 interface LoginModalProps {
     className?: string
@@ -14,7 +13,6 @@ export const LoginModal = ({ className, onClose, isOpen }: LoginModalProps) => {
     const { t } = useTranslation('');
     return (
         <Modal
-            className={classNames(s.LoginModal, {}, [className])}
             isOpen={isOpen}
             onClose={onClose}
             lazy
